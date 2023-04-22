d6954280a97f86961ee3d250a77ebc8a7858f77c
Julian Gorski <unconfigured@null.spigotmc.org>
2023-04-22 13:19:57 +0200

    Added additional checking to register request like password strenght and whether email address is correct.

--- NawigacjaSklepowaAPI/Services/AuthService.cs
+++ NawigacjaSklepowaAPI/Services/AuthService.cs
@@ -18,10 +18,35 @@ namespace NawigacjaSklepowaAPI.Services
             _mapper = mapper;
         }
 
-        public async Task<(bool IsUserRegistered, string Message)> Register(UserRegistrationDto request) 
+        public async Task<(bool IsUserRegistered, string Message)> Register(UserRegistrationDto request)
         {
+            // Check if password is strong enough
+            var min_chars = 8;
+            var max_chars = 32;
+
+            if (request.Password.Length < min_chars)
+                return (false, $"Hasło musi mieć co najmniej {min_chars} znaków.");
+
+            if (request.Password.Length > max_chars)
+                return (false, $"Hasło może mieć maksymalnie {max_chars} znaki.");
+
+            if (!request.Password.Any(char.IsUpper))
+                return (false, "Hasło musi zawierać co najmniej jedną wielką literę.");
+
+            if (!request.Password.Any(char.IsLower))
+                return (false, "Hasło musi zawierać co najmniej jedną małą literę.");
+
+            if (!request.Password.Any(char.IsDigit))
+                return (false, "Hasło musi zawierać co najmniej jedną cyfrę.");
+
+            // Check if passwords match
+            if (request.Password != request.ConfirmPassword)
+                return (false, "Hasła nie są takie same.");
+
+            // Check if user with given email already exists
             if (_context.Users.Any(u => u.Email.ToLower() == request.Email.ToLower()))
-                return (false, "Użytkownik z takim emailem już istnieje");
+                return (false, "Użytkownik z takim emailem już istnieje.");
+
 
             User user = _mapper.Map<User>(request);
             user.Password = BCrypt.Net.BCrypt.HashPassword(request.Password);
